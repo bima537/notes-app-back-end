a955041810b8a931e98fe8caa0370f8370c114ca
bima537 <bima537@github.com>
2026-05-12 23:53:06 +0800

    feat: add cors support

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import ErrorHandler from '../middlewares/error.js';
 import routes from '../routes/index.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(ErrorHandler);
